acfaec7e398acf0c8fe548147c192125bfac108d
Xiomara Chunga Carrillo <geraldyne.110101@gmail.com>
2022-08-25 18:02:46 -0500

    Initial Commit

---
+++ ChallengeCeibaiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChallengeCeibaiOS
+//
+//  Created by Xio iOS on 25/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
