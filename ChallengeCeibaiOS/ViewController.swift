fe2de811870673d04555ce7c1e1ad01ea515b6b5
Xiomara Chunga Carrillo <geraldyne.110101@gmail.com>
2022-08-26 11:51:23 -0500

    backgroundLoading

--- ChallengeCeibaiOS/ViewController.swift
+++ ChallengeCeibaiOS/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        LoadingViewprueba.isHidden = true
+        LoadingViewprueba.isHidden = false
         // Do any additional setup after loading the view.
          //color, number of circles, duration, growColor.
      }
