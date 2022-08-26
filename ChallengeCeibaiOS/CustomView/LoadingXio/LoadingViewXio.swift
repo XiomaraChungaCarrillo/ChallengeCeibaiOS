5b999f147b692fefce7301fd263c144d57ee505a
Xiomara Chunga Carrillo <geraldyne.110101@gmail.com>
2022-08-26 15:33:45 -0500

    testLoading

--- ChallengeCeibaiOS/CustomView/LoadingXio/LoadingViewXio.swift
+++ ChallengeCeibaiOS/CustomView/LoadingXio/LoadingViewXio.swift
@@ -7,13 +7,14 @@
 
 import UIKit
 
+protocol CustomLoadingViewwDelegate: class {
+    func startAnimation()
+    func finishAnimation()
+}
 
 
-@IBDesignable
 class LoadingViewXio: UIView {
-    
-    weak var insideview: UIView?
-    
+        
     // STORYBOARD
     @IBOutlet var containerView: UIView!
     @IBOutlet var loadingView: UIView!
@@ -21,17 +22,16 @@ class LoadingViewXio: UIView {
     @IBOutlet var circleB: UIView!
     @IBOutlet var circleC: UIView!
     @IBOutlet var circleD: UIView!
-    @IBOutlet weak var heighLoading: NSLayoutConstraint!
-    @IBOutlet weak var widthLoading: NSLayoutConstraint!
+  
     
-    
-    
-    //Inspectable
-    @IBInspectable var isHiddenLoading: Bool = true
+    weak var delegate: CustomLoadingViewwDelegate?
+
     
     //MARK: Loading Properties
     var heightNumberLoading: Int = 250
     var widthNumberLoading: Int = 250
+    
+    
     lazy var circles: [UIView] = [circleA, circleB, circleC, circleD]
     var circle = 0
     var colors: [UIColor] = [#colorLiteral(red: 0.32390064, green: 0.4138930738, blue: 0.9091263413, alpha: 0.5), #colorLiteral(red: 0.8235835433, green: 0.5749723315, blue: 0, alpha: 0.5), #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 0.5), #colorLiteral(red: 0.09879464656, green: 0.3816201091, blue: 0.2502036691, alpha: 0.5), #colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 0.5), #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 0.5), #colorLiteral(red: 1, green: 0.2204911709, blue: 0.2471658289, alpha: 0.5), #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 0.5), #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 0.5), #colorLiteral(red: 1, green: 0.71805197, blue: 1, alpha: 0.5)]
@@ -46,78 +46,17 @@ class LoadingViewXio: UIView {
         return blurEffectView
     }()
     
-    //MARK: Init
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        initialize()
-    }
-    
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        initialize()
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        setupTheme()
-    }
-    
     override func awakeFromNib() {
-        
+        super.awakeFromNib()
     }
     
-}
+    func starAnimation(){
+        loading = true
+        setupLoadingView()
+    }
 
-
-//MARK: - SetupView
-extension LoadingViewXio {
-    
-    //MARK: Initialize
-    private func initialize(){
-        insideview = loadViewFromNib()
-        setupUI(view: insideview ?? UIView())
-    }
-    
-    //MARK: UI Methods
-    private func loadViewFromNib() -> UIView! {
-        let bundle = Bundle(for: type(of: self))
-        let nib = UINib(nibName: String(describing: type(of: self)),
-                        bundle: bundle)
-        return nib.instantiate( withOwner: self,
-                                options: nil).first as? UIView
-    }
-    
-    private func setupUI(view: UIView?){
-        insideview?.frame = self.bounds
-        addSubview(insideview ?? UIView())
-        clearConstraints()
-        
-    }
-    
-    
-    private func clearConstraints(){
-        insideview?.translatesAutoresizingMaskIntoConstraints = false
-        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[childView]|",
-                                                      options: [],
-                                                      metrics: nil,
-                                                      views: ["childView": insideview ?? UIView()]))
-        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[childView]|",
-                                                      options: [],
-                                                      metrics: nil,
-                                                      views: ["childView": insideview ?? UIView()]))
-    }
-}
-
-//MARK: - ShowLoading
-extension LoadingViewXio {
-    
-    private func setupTheme() {
-        heighLoading.constant = CGFloat(heightNumberLoading)
-        widthLoading.constant = CGFloat(widthNumberLoading)
-        setUpCircles()
-        
-        loading = isHiddenLoading
-        
+    func finishAnimation(){
+        loading = false
         setupLoadingView()
     }
 }
@@ -137,12 +76,16 @@ extension LoadingViewXio {
     func blurView(completion: @escaping (_ success: Bool) -> ()) {
         
         if !UIAccessibility.isReduceTransparencyEnabled {
-            loadingView.addSubview(blurry)
-            loadingView.bringSubviewToFront(blurry)
+//            loadingView.backgroundColor = #colorLiteral(red: 0.01176470588, green: 0.03137254902, blue: 0.1843137255, alpha: 1)
+//            loadingView.addSubview(blurry)
+//            loadingView.bringSubviewToFront(blurry)
             loadingView.bringSubviewToFront(containerView)
+//            loadingView.backgroundColor = #colorLiteral(red: 0.01176470588, green: 0.03137254902, blue: 0.1843137255, alpha: 1)
             UIView.animate(withDuration: 0.6) {
                 self.blurry.alpha = 0.5
-                completion(true) }}}
+                completion(true) }
+        }
+    }
     
     func nextCircle() {
         
--- ChallengeCeibaiOS/ViewController.swift
+++ ChallengeCeibaiOS/ViewController.swift
@@ -9,20 +9,36 @@ import UIKit
  
 class ViewController: UIViewController {
     
-  
-    @IBOutlet weak var LoadingViewprueba: LoadingViewXio!
-    
-
     
  
-
+    @IBOutlet weak var viewTest: UIView!
+    
+    private let startLoading = UINib(nibName: "LoadingViewXio", bundle: nil).instantiate(withOwner: self, options: nil).first as! LoadingViewXio
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        LoadingViewprueba.isHidden = false
-        // Do any additional setup after loading the view.
-         //color, number of circles, duration, growColor.
+        startAnimation()
      }
+    
+    func startAnimation() {
+        finishAnimation()
+        self.view.addSubview(startLoading)
+        setConstraints(forView: startLoading, toView: self.view)
+        startLoading.starAnimation()
+    }
+    
+    func finishAnimation() {
+        startLoading.finishAnimation()
+        startLoading.removeFromSuperview()
+    }
 
+    private func setConstraints(forView view: UIView,toView:UIView) {
+        view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint(item: view, attribute: .top, relatedBy: .equal, toItem: toView, attribute: .top, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: view, attribute: .bottom, relatedBy: .equal, toItem: toView, attribute: .bottom, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: view, attribute: .left, relatedBy: .equal, toItem: toView, attribute: .left, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: view, attribute: .right, relatedBy: .equal, toItem: toView, attribute: .right, multiplier: 1, constant: 0).isActive = true
+    }
 
 }
 
